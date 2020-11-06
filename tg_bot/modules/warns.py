70c2d3934c310f7d7861b993e1b42210252ae252
Saprativ Basak <skievoid@gmail.com>
2020-11-06 09:02:07 +0530

    Update warns.py

--- tg_bot/modules/warns.py
+++ tg_bot/modules/warns.py
@@ -400,9 +400,9 @@ __help__ = """
  - /warn <userhandle>: warn a user. After 3 warns, the user will be banned from the group. Can also be used as a reply.
  - /resetwarn <userhandle>: reset the warnings for a user. Can also be used as a reply.
  - /addwarn <keyword> <reply message>: set a warning filter on a certain keyword. If you want your keyword to \
-be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is an angry user`. 
- - /nowarn <keyword>: stop a warning filter
- - /warnlimit <num>: set the warning limit
+be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is an angry user.
+ - /nowarn <keyword>: stop a warning filter.
+ - /warnlimit <num>: set the warning limit.
  - /strongwarn <on/yes/off/no>: If set to on, exceeding the warn limit will result in a ban. Else, will just kick.
 """
 
